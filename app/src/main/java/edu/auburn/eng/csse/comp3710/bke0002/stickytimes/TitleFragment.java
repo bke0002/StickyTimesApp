61e9387d02c450a04040b612d03f3cf5c62c1aa6
bke0002 <bke0002@auburn.edu>
2016-04-04 20:07:41 -0500

    Initial commit

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/bke0002/stickytimes/TitleFragment.java
@@ -0,0 +1,31 @@
+package edu.auburn.eng.csse.comp3710.bke0002.stickytimes;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TitleFragment extends Fragment {
+
+
+    public TitleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_title, container, false);
+
+        return v;
+    }
+
+}
